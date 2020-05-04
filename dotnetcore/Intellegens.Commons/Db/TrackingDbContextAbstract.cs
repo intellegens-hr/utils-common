c05dc4f75853fccadea19d56719923ebf56405e6
Drazen Mrvos <dmrvos@gmail.com>
2020-05-04 11:47:41 +0200

    Added TrackingDbContextExtensions which can be used when DbContext needs to inherit from other DbContext derived class (like IdentityDbContext)

--- dotnetcore/Intellegens.Commons/Db/TrackingDbContextAbstract.cs
+++ dotnetcore/Intellegens.Commons/Db/TrackingDbContextAbstract.cs
@@ -22,55 +22,27 @@ namespace Intellegens.Commons.Db
             this.userData = userData;
         }
 
-        private void SetEntityTrackingData()
-        {
-            var entriesToCheck = base.ChangeTracker
-                .Entries()
-                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified)
-                .Where(x => x.Entity is BaseEntityAbstract)
-                .Select(x => new
-                {
-                    x.State,
-                    Entity = x.Entity as BaseEntityAbstract
-                });
-
-            foreach (var entry in entriesToCheck)
-            {
-                if (entry.State == EntityState.Added)
-                {
-                    entry.Entity.TimeCreated = DateTime.UtcNow;
-                    entry.Entity.UserCreatedId = userData.GetUserId();
-                }
-
-                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
-                {
-                    entry.Entity.TimeUpdated = DateTime.UtcNow;
-                    entry.Entity.UserUpdatedId = userData.GetUserId();
-                }
-            }
-        }
-
         public override int SaveChanges()
         {
-            SetEntityTrackingData();
+            this.SetEntityTrackingData(userData);
             return base.SaveChanges();
         }
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-            SetEntityTrackingData();
+            this.SetEntityTrackingData(userData);
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            SetEntityTrackingData();
+            this.SetEntityTrackingData(userData);
             return base.SaveChangesAsync(cancellationToken);
         }
 
         public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
         {
-            SetEntityTrackingData();
+            this.SetEntityTrackingData(userData);
             return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
     }
---
+++ dotnetcore/Intellegens.Commons/Db/TrackingDbContextExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Intellegens.Commons.Db.BaseEntities;
+using Intellegens.Commons.Db.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Intellegens.Commons.Db
+{
+    public static class TrackingDbContextExtensions
+    {
+        /// <summary>
+        /// In context's change tracker, goes through all entities and sets tracking data (user/date created/modified)
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="userData"></param>
+        public static void SetEntityTrackingData(this DbContext dbContext, IUserData userData)
+        {
+            var entriesToCheck = dbContext.ChangeTracker
+                .Entries()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified)
+                .Where(x => x.Entity is BaseEntityAbstract)
+                .Select(x => new
+                {
+                    x.State,
+                    Entity = x.Entity as BaseEntityAbstract
+                });
+
+            foreach (var entry in entriesToCheck)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.TimeCreated = DateTime.UtcNow;
+                    entry.Entity.UserCreatedId = userData.GetUserId();
+                }
+
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                {
+                    entry.Entity.TimeUpdated = DateTime.UtcNow;
+                    entry.Entity.UserUpdatedId = userData.GetUserId();
+                }
+            }
+        }
+    }
+}
