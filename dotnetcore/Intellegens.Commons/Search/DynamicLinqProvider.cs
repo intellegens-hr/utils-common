66827279f9daaafb36458b59a0eb08dfb8b35404
Drazen Mrvos <dmrvos@gmail.com>
2020-11-11 16:29:22 +0100

    Upgrade to .Net 5, implement DynamicLinqProvider based on official implementation.

--- dotnetcore/Intellegens.Commons/Search/DynamicLinqProvider.cs
+++ dotnetcore/Intellegens.Commons/Search/DynamicLinqProvider.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Linq.Dynamic.Core.CustomTypeProviders;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Intellegens.Commons.Search
 {
     /// <summary>
-    /// Used by dynamic Linq library to enable support for Db functions inside dynamic queries
+    /// Used by dynamic Linq library to enable support for Db functions inside dynamic queries.
+    /// https://github.com/zzzprojects/System.Linq.Dynamic.Core/blob/master/src/System.Linq.Dynamic.Core/CustomTypeProviders/DefaultDynamicLinqCustomTypeProvider.cs
     /// </summary>
-    public class DynamicLinqProvider : IDynamicLinkCustomTypeProvider
+    public class DynamicLinqProvider : AbstractDynamicLinqCustomTypeProvider, IDynamicLinkCustomTypeProvider
     {
         public HashSet<Type> GetCustomTypes()
         {
@@ -21,14 +26,27 @@ namespace Intellegens.Commons.Search
             return types;
         }
 
-        public Type ResolveType(string typeName)
+        public Dictionary<Type, List<MethodInfo>> GetExtensionMethods()
         {
-            throw new NotImplementedException();
+            var types = GetCustomTypes();
+
+            List<Tuple<Type, MethodInfo>> list = new List<Tuple<Type, MethodInfo>>();
+
+            foreach (var type in types)
+            {
+                var extensionMethods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                    .Where(x => x.IsDefined(typeof(ExtensionAttribute), false)).ToList();
+
+                extensionMethods.ForEach(x => list.Add(new Tuple<Type, MethodInfo>(x.GetParameters()[0].ParameterType, x)));
+            }
+
+            return list.GroupBy(x => x.Item1, tuple => tuple.Item2).ToDictionary(key => key.Key, methods => methods.ToList());
         }
 
+        public Type ResolveType(string typeName)
+            => ResolveType(AppDomain.CurrentDomain.GetAssemblies(), typeName);
+
         public Type ResolveTypeBySimpleName(string simpleTypeName)
-        {
-            throw new NotImplementedException();
-        }
+            => ResolveTypeBySimpleName(AppDomain.CurrentDomain.GetAssemblies(), simpleTypeName);
     }
 }
